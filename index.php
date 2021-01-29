51dd9fcc61af05693d4a6c8e79f4791340f42a73
jcigama <jcigama77@yahoo.com>
2021-01-29 15:08:08 -0500

    Made and defined config file

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+//Turn on error reporting -- this is critical!
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require the autoload file
+require_once('vendor/autoload.php');
+
+//Create an instance of the Base class
+$f3 = Base::instance();
+$f3->set('DEBUG', 3);
+
+//Default Route
+$f3->route('GET /', function() {
+    echo "My Pets";
+//    $view = new Template();
+//    echo $view->render('views/home.html');
+});
+
+
+//Run fat free
+$f3->run();
